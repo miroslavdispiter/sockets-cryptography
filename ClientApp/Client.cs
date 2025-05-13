d9d0718ff6984fdba55fdd4f1294fdfb6de9d29d
miroslavdispiter <miroslav.dispiter.it@gmail.com>
2025-05-13 20:56:39 +0200

    Add project files.

---
+++ ClientApp/Client.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientApp
+{
+    internal class Client
+    {
+        static void Main(string[] args)
+        {
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint destinationEP = new IPEndPoint(IPAddress.Loopback, 50001);
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Unesite poruku: ");
+                    string poruka = Console.ReadLine();
+                    byte[] binarnaPoruka = Encoding.UTF8.GetBytes(poruka);
+                    clientSocket.SendTo(binarnaPoruka, 0, binarnaPoruka.Length, SocketFlags.None, destinationEP);
+                    Console.WriteLine($"Klijent poslao poruku na: {destinationEP}");
+                }
+                catch (SocketException ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+        }
+    }
+}
---
+++ ServerApp/Server.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerApp
+{
+    internal class Server
+    {
+        static void Main(string[] args)
+        {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint serverEP = new IPEndPoint(IPAddress.Any, 50001);
+            serverSocket.Bind(serverEP);
+
+            EndPoint senderEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            byte[] prijemniBafer = new byte[1024];
+
+            while (true)
+            {
+                try
+                {
+                    int brBajta = serverSocket.ReceiveFrom(prijemniBafer, ref senderEndPoint);
+                    string poruka = Encoding.UTF8.GetString(prijemniBafer, 0, brBajta);
+                    Console.WriteLine($"Server primio poruku: {poruka}");
+                }
+                catch (SocketException ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+        }
+    }
+}
