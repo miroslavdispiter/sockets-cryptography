046998ed1060c0c722e63d3b747dae94fe74e892
miroslavdispiter <miroslav.dispiter.it@gmail.com>
2025-05-14 23:01:34 +0200

    dodata osnovna implementacija klijentske aplikacije sa izborom protokola i algoritma

--- ClientApp/Client.cs
+++ ClientApp/Client.cs
@@ -12,24 +12,55 @@ namespace ClientApp
     {
         static void Main(string[] args)
         {
-            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            IPEndPoint destinationEP = new IPEndPoint(IPAddress.Loopback, 50001);
+            Socket clientSocket = null;
+            IPEndPoint serverEP = new IPEndPoint(IPAddress.Loopback, 50001);
+            string algoritam = string.Empty;
 
             while (true)
             {
-                try
+                Console.WriteLine("Koji protokol koristi server? (TCP ili UDP): ");
+                string protokol = Console.ReadLine();
+
+                if (protokol.ToLower() == "tcp")
                 {
-                    Console.WriteLine("Unesite poruku: ");
-                    string poruka = Console.ReadLine();
-                    byte[] binarnaPoruka = Encoding.UTF8.GetBytes(poruka);
-                    clientSocket.SendTo(binarnaPoruka, 0, binarnaPoruka.Length, SocketFlags.None, destinationEP);
-                    Console.WriteLine($"Klijent poslao poruku na: {destinationEP}");
+                    clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                    clientSocket.Connect(serverEP);
+                    Console.WriteLine("Klijent je uspesno povezan sa serverom!");
+
+                    break;
                 }
-                catch (SocketException ex)
+                else if (protokol.ToLower() == "udp")
                 {
-                    Console.WriteLine(ex);
+                    clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                    Console.WriteLine("Mozete slati poruke preko UDP protokola!");
+
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Uneli ste pogresan naziv protokola, pokusajte ponovo");
+                    continue;
                 }
             }
+
+            while (true)
+            {
+                Console.WriteLine("Koji algoritam za sifrovanje ce se koristiti? (DES ili RSA): ");
+                algoritam = Console.ReadLine();
+
+                if (algoritam.ToLower() == "des" || algoritam.ToLower() == "rsa")
+                {
+                    Console.WriteLine($"Izabrani algoritam: {algoritam}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Uneli ste pogresan naziv algoritma za sifrovanje, pokusajte ponovo");
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
